56c1bc43d2ee53ba21952c70324e769fc1ea5147
yonatanmk <yonatanmk@gmail.com>
2023-02-17 20:40:02 -0800

    ability bugfix

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,5 +1,6 @@
 import capitalize from 'lodash/capitalize';
-import type { IPokemonRow, IPokemonQueryDatum, ITypeQueryDatum, IType, ISortField } from '../interfaces';
+import uniqBy from 'lodash/uniqBy';
+import type { IPokemonRow, IPokemonQueryDatum, ITypeQueryDatum, IType, ISortField, IPokemonQueryAbility } from '../interfaces';
 
 export const PAGE_SIZE = 20;
 
@@ -25,12 +26,13 @@ export const formatPokemonRow = (pokemonData: IPokemonQueryDatum): IPokemonRow =
     },
     abilities: {
       props: {
-        abilities: pokemon_v2_pokemonabilities.map(ability => ({
-          id: ability.id,
-          name: ability.pokemon_v2_ability?.name,
-          effect: ability.pokemon_v2_ability?.pokemon_v2_abilityeffecttexts[0]?.effect,
-          short_effect: ability.pokemon_v2_ability?.pokemon_v2_abilityeffecttexts[0]?.short_effect,
-        }))
+        abilities: uniqBy(pokemon_v2_pokemonabilities, (ability: IPokemonQueryAbility) => ability.pokemon_v2_ability.id)
+          .map((ability: IPokemonQueryAbility) => ({
+            id: ability.id,
+            name: ability.pokemon_v2_ability?.name,
+            effect: ability.pokemon_v2_ability?.pokemon_v2_abilityeffecttexts[0]?.effect,
+            short_effect: ability.pokemon_v2_ability?.pokemon_v2_abilityeffecttexts[0]?.short_effect,
+          }))
       }
     },
     types: {
