11c0de0df2fd9f9d93223fce9a6dd3928601453a
yonatanmk <yonatanmk@gmail.com>
2023-02-19 14:18:04 -0800

    FilterPanel component

---
+++ src/components/FilterPanel/FilterPanel.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import classnames from 'classnames';
+import debounce from 'lodash/debounce';
+import { BsSearch } from "react-icons/bs";
+import type { IType } from '../../interfaces';
+import styles from './FilterPanel.module.scss';
+
+export type IFilterPanelProps = {
+  resultsCount: number;
+  allTypes: IType[];
+  selectedTypes: number[];
+  onSearchChange: (newSearch: string) => void;
+  onSelectedTypesChange: (newTypeIds: number[]) => void;
+}
+
+function FilterPanel({ 
+  resultsCount,
+  allTypes,
+  selectedTypes,
+  onSearchChange,
+  onSelectedTypesChange,
+}: IFilterPanelProps) {
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleSearchChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setSearchInput(e.currentTarget.value);
+    debouncedSetSearch(e.currentTarget.value)
+  }
+
+  const debouncedSetSearch = debounce(onSearchChange, 500);
+
+  const toggleTypeSelected = (typeId: number) => {
+    const newSelectedTypes = selectedTypes.includes(typeId) ? selectedTypes.filter(id => id !== typeId) : [...selectedTypes, typeId];
+    onSelectedTypesChange(newSelectedTypes)
+  };
+
+  return (
+    <>
+      <div className={classnames(styles["FilterPanel__Row--head"], styles["FilterPanel__Row--label"])}>
+        <p>{resultsCount} Matching Pokémon</p>
+      </div>
+      <div className={styles.FilterPanel__Row}>
+        <div className={styles.FilterPanel__Search}>
+          <input id="people-search" type="text" value={searchInput} onChange={handleSearchChange} placeholder="Search Pokémon Name" />
+          <BsSearch />
+        </div>
+      </div>
+      <div className={classnames(styles.FilterPanel__Row, styles.CheckboxList)}>
+        {allTypes && allTypes.map(type => (
+          <div className={styles.Checkbox} key={type.id}>
+            <input 
+              className={styles.Checkbox__checkbox} 
+              type="checkbox" 
+              checked={selectedTypes.includes(type.id)} 
+              onChange={() => toggleTypeSelected(type.id)}
+            />
+            <label className={styles.Checkbox__label}>{type.name}</label>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default FilterPanel;
--- src/layouts/App/App.tsx
+++ src/layouts/App/App.tsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from 'react';
-import debounce from 'lodash/debounce';
 import styles from './App.module.scss';
-import { BsSearch } from "react-icons/bs";
 import { useQuery } from "@apollo/client"
-import classnames from 'classnames';
 import Table from '../../components/Table';
 import Loading from '../../components/Loading';
+import FilterPanel from '../../components/FilterPanel';
 import InfoPanel from '../../components/InfoPanel';
 import { formatPokemonRow, formatPokemonType, PAGE_SIZE, SORT_FIELDS, pokemonColumns } from '../../util';
 import type { IPokemonRow, IType, ISortOrder, ISortField, ITypeQueryData } from '../../interfaces';
@@ -16,7 +14,6 @@ import { GET_POKEMON_TYPES } from '../../graphql/queries';
 function App() {
   const [pokemonRows, setPokemonRows] = useState<IPokemonRow[]>([]);
   const [page, setPage] = useState<number>(0);
-  const [searchInput, setSearchInput] = useState('');
   const [search, setSearch] = useState('');
   const [resultsCount, setResultsCount] = useState(0);
   const [allTypes, setAllTypes] = useState<IType[]>([]);
@@ -27,10 +24,9 @@ function App() {
   const [loading, setLoading] = useState(false);
 
   const { data: typeData, loading: typeLoading, error: typeError } = useQuery<ITypeQueryData>(GET_POKEMON_TYPES);
-
   const { data: pokemonData, loading: pokemonLoading, error: pokemonError, loadPokemon } = usePokemonQuery(); 
-
-  const isLoading = loading || typeLoading || pokemonLoading
+  const isLoading = loading || typeLoading || pokemonLoading;
+  const onLastPage = pokemonRows.length === resultsCount;
 
   useEffect(() => {
     if (pokemonData && !pokemonLoading && !pokemonError) {
@@ -90,7 +86,6 @@ function App() {
       sortField: sortFieldArg || sortField,
       selectedTypes: selectedTypesArg || (selectedTypes.length == 0 ? allTypes.map(type => type.id) : selectedTypes),
     })
-
   }
 
   const loadNextPage = () => {
@@ -100,27 +95,19 @@ function App() {
     })
   }
 
-  const handleSearchChange = (e: React.FormEvent<HTMLInputElement>) => {
-    setSearchInput(e.currentTarget.value);
-    debouncedSetSearch(e.currentTarget.value)
-  }
-
-  const debouncedSetSearch = debounce(
-    (newSearch: string) => {
-      setSearch(newSearch)
-      refetch({
-        reset: true,
-        nameSearch: `%${newSearch}%`,
-      })
-    }, 500)
-
-  const toggleTypeSelected = (typeId: number) => {
-    console.log('toggleTypeSelected')
-    const newSelectedTypes = selectedTypes.includes(typeId) ? selectedTypes.filter(id => id !== typeId) : [...selectedTypes, typeId]
-    setSelectedTypes(newSelectedTypes)
+  const onSelectedTypesChange = (newTypeIds: number[]) => {
+    setSelectedTypes(newTypeIds)
     refetch({
       reset: true,
-      selectedTypes: newSelectedTypes.length == 0 ? allTypes.map(type => type.id) : newSelectedTypes,
+      selectedTypes: newTypeIds.length == 0 ? allTypes.map(type => type.id) : newTypeIds,
+    })
+  }
+
+  const onSearchChange = (newSearch: string) => {
+    setSearch(newSearch)
+    refetch({
+      reset: true,
+      nameSearch: `%${newSearch}%`,
     })
   }
 
@@ -141,8 +128,6 @@ function App() {
     })
   }
 
-  const onLastPage = pokemonRows.length === resultsCount;
-
   return (
     <div className={styles.App}>
       <div className={styles.Header}>
@@ -150,28 +135,13 @@ function App() {
       </div>
       <div className={styles.App__Body}>
         <div className={styles.App__Sidebar}>
-          <div className={classnames(styles["App__Sidebar__Row--head"], styles["App__Sidebar__Row--label"])}>
-            <p>{resultsCount} Matching Pokémon</p>
-          </div>
-          <div className={styles.App__Sidebar__Row}>
-            <div className={styles.App__Sidebar__Search}>
-              <input id="people-search" type="text" value={searchInput} onChange={handleSearchChange} placeholder="Search Pokémon Name" />
-              <BsSearch />
-            </div>
-          </div>
-          <div className={classnames(styles.App__Sidebar__Row, styles.CheckboxList)}>
-            {allTypes && allTypes.map(type => (
-              <div className={styles.Checkbox} key={type.id}>
-                <input 
-                  className={styles.Checkbox__checkbox} 
-                  type="checkbox" 
-                  checked={selectedTypes.includes(type.id)} 
-                  onChange={() => toggleTypeSelected(type.id)}
-                />
-                <label className={styles.Checkbox__label}>{type.name}</label>
-              </div>
-            ))}
-          </div>
+          <FilterPanel
+            resultsCount={resultsCount}
+            allTypes={allTypes}
+            selectedTypes={selectedTypes}
+            onSearchChange={onSearchChange}
+            onSelectedTypesChange={onSelectedTypesChange}
+          />
           <InfoPanel selectedPokemon={selectedPokemon} />
         </div>
         <div className={styles.App__Content}>
